dd78023952a402337815f4c6ec63931dd0613938
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-01-04 11:54:32 +0100

    Add missing VCPKG_TARGET_TRIPLET

--- build_with_vcpkg.bat
+++ build_with_vcpkg.bat
@@ -4,5 +4,5 @@ call vcpkg\bootstrap-vcpkg.bat
 .\vcpkg\vcpkg install fftw3:x64-windows-static
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake ..
 cmake --build .
